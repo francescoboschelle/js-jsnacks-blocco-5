778ed7144652ce4af1e4813c74d3ae6db0f88327
francescoboschelle <francibossch@gmail.com>
2025-06-13 13:35:25 +0200

    Snack 05 completed by creating new array with even nums

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const arr = [];
+nums.forEach((num) => {
+  if (num % 2 === 0) {
+    arr.push(num);
+  }
+});
+
+console.log(arr);
